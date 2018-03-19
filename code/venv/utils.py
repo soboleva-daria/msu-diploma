bb2762711a048654e1a1e919abc1acb0ea47b9fc
daria soboleva <daria_soboleva@darias-MacBook-Air.local>
2018-03-19 10:24:32 +0300

    Added Interactions and Word2Vec.

--- code/venv/utils.py
+++ code/venv/utils.py
@@ -1,9 +1,11 @@
 from pymystem3 import Mystem
-from polyglot.text import Text
+#from polyglot.text import Text
+import gensim
 from nltk.corpus import stopwords
 from string import punctuation
 from nltk.tokenize import word_tokenize
 from textblob import TextBlob as tb
+from natasha import DatesExtractor
 from collections import Counter
 import pymorphy2
 import pandas as pd
@@ -11,6 +13,12 @@ import numpy as np
 import math
 from tqdm import tqdm, tqdm_pandas
 from sklearn.model_selection import KFold
+from sklearn.metrics import roc_auc_score
+from scipy.spatial.distance import cosine
+import os
+from copy import copy
+from joblib import Parallel, delayed
+import multiprocessing
 
 
 class Utility(object):
@@ -24,6 +32,10 @@ class Utility(object):
         Utility.morph = pymorphy2.MorphAnalyzer()
 
     @staticmethod
+    def set_dates_extractor():
+        Utility.dates_extractor = DatesExtractor()
+   
+    @staticmethod
     def set_stop_words():
         Utility.stop_words = stopwords.words('russian')
 
@@ -32,32 +44,14 @@ class Utility(object):
         Utility.interrogative_pronouns = {'кто', 'что', 'какой', 'чей', 'который', 'сколько', 'когда', 'где', 'куда', 'как', 'откуда', 'почемy', 'зачем'}
 
     @staticmethod
-    def get_interrogative_pronouns(question):
-        if not hasattr(Utility, 'interrogative_pronouns'):
-            Utility.set_interrogative_pronouns()
-        if not hasattr(Utility, 'stem'):
-            Utility.set_stem()
-        stem = Utility.stem
-        interrogative_pronouns = Utility.interrogative_pronouns
+    def set_w2v():
+        # Load Google's pre-trained Word2Vec model.
+        #model = gensim.models.KeyedVectors.load_word2vec_format('ruwikiruscorpora_upos_skipgram_300_2_2018.vec')  
 
-        for w in tb(question).words:
-            w_lem = ''.join(stem.lemmatize(w.lower())).replace('\n', '')
-            if w_lem in interrogative_pronouns:
-                return w_lem
-
-    @staticmethod
-    def get_speech_part(word):
-        if not hasattr(Utility, 'morph'):
-            Utility.set_morph()
-        return str(Utility.morph.parse(word)[0].tag).split(',')[0]
-
-    @staticmethod
-    def get_ner(word):
-        if word.replace('.', '').replace(',', '').isdigit():
-            return 'I-NUM'
-        entities = Text(word, hint_language_code='ru').entities
-        if len(entities):
-            return entities[0].tag
+        #w2v = {}
+        #for i in model.vocab:
+        #    w2v[i.split('_')[0]] = model[i]
+        Utility.w2v = np.load('w2v.npy').item()
 
     @staticmethod
     def lemmatize(text):
@@ -147,103 +141,178 @@ class Utility(object):
             sentence_len.append(len(sentence_in_words))
             unique_word_count_scores.append(len(question_in_words.intersection(sentence_in_words)))
             unique_word_percent_scores.append(unique_word_count_scores[-1] / len_question_in_words)
-
             bm25f_scores.append(Utility.calc_bm25f_score(question_in_words, sentence_in_words, idfs))
             tf_idf_scores.append(Utility.calc_tf_idf_score(question_in_words, sentence_in_words, idfs))
 
         return unique_word_count_scores, unique_word_percent_scores, sentence_len, bm25f_scores, tf_idf_scores
 
     @staticmethod
-    def get_most_frequent_words_ner_and_speech_part(bloblist):
-        if not hasattr(Utility, 'stop_words'):
-            Utility.set_stop_words()
+    def get_answer_by_score(data, col_with_score):
+        return roc_auc_score(data.answer_in_sentence, data[col_with_score])
+
+    @staticmethod
+    def get_word_ner(word):
+        if word.replace('.', '').replace(',', '').isdigit():
+            return 'I-NUM'
+        entities = Text(word, hint_language_code='ru').entities
+        if len(entities):
+            return entities[0].tag
+
+    @staticmethod
+    def dump_sentences_ners(sentences):
+        ners = []
+        for sentence in sentences:
+            ners.append(Utility.get_sentence_ners(sentence))
+        s = pd.Series([sentences, ners])
+        return pd.DataFrame.from_items(zip(s.index, s.values))
+	
+    @staticmethod
+    def applyParallel(dfGrouped, func):
+        retLst = Parallel(n_jobs=multiprocessing.cpu_count())(delayed(func)(group) for name, group in tqdm(dfGrouped, total=len(dfGrouped)))
+        return pd.concat(retLst)
+
+    @staticmethod
+    def get_question_type(df):
+        if not hasattr(Utility, 'interrogative_pronouns'):
+            Utility.set_interrogative_pronouns()
         if not hasattr(Utility, 'morph'):
             Utility.set_morph()
-        stop_words = Utility.stop_words
-        stop_words += ['—']
+        if not hasattr(Utility, 'stop_words'):
+            Utility.set_stop_words()
+        interrogative_pronouns = Utility.interrogative_pronouns
         morph = Utility.morph
+        stop_words = Utility.stop_words
 
-        words = []
-        for blob in bloblist:
-            words.extend(list(filter(lambda w: w not in stop_words, tb(blob.lower()).words)))
-        most_common_words = [t[0] for t in Counter(words).most_common(10)]
-        tags = set()
-        speech_parts = set()
-        for word in most_common_words:
-            speech_parts.add(Utility.get_speech_part(word))
-            tags.add(Utility.get_ner(word))
-        return pd.Series([tags, speech_parts])
-
-    @staticmethod
-    def count_target_statistic(df_train, df_test, target):
-        tqdm_pandas(tqdm(total=df_train.question_type.nunique()))
-        res = df_train.groupby('question_type').progress_apply(lambda x: Utility.get_most_frequent_words_ner_and_speech_part(x[target])).reset_index()
-        res.columns = [
-            'question_type',
-            'most_frequent_{}_ner_by_question_type'.format(target),
-            'most_frequent_{}_speech_type_by_question_type'.format(target)
-        ]
-        return pd.merge(df_test.reset_index(), res, how='left', on='question_type')
+        question_id = df.index.values[0]
+        question = df.question.values[0]
+    
+        question_interrogative_pronoun = ''
+        for w in tb(question).words:
+            w_lem = morph.parse(w)[0].normal_form
+            if len(question_interrogative_pronoun) and (w_lem not in stop_words):
+                df.loc[question_id, 'question_type'] = '{} {}'.format(question_interrogative_pronoun, w_lem)
+                return df
+            if w_lem in interrogative_pronouns:
+                question_interrogative_pronoun = w_lem
+        df.loc[question_id, 'question_type'] = question_interrogative_pronoun
+        return df
 
     @staticmethod
-    def count_target_statistic_by_folds(df_train, target, n_splits):
-        res = []
-        questions = df_train.index.unique()
-        for i, j in tqdm(KFold(n_splits=n_splits, random_state=0).split(questions), total=n_splits):
-            df_i, df_j = df_train.loc[questions[i]], df_train.loc[questions[j]]
-            res_by_fold = Utility.count_target_statistic(df_i, df_j, target)
-            res.append(res_by_fold)
-        return pd.concat(res)
+    def get_sentence_ners(df):
+        if not hasattr(Utility, 'morph'):
+            Utility.set_morph()
+        if not hasattr(Utility, 'dates_extractor'):
+            Utility.set_dates_extractor()
+        morph = Utility.morph
+        dates_extractor = Utility.dates_extractor
 
-    @staticmethod
-    def get_speech_type_target_stat(question_id, sentences, speech_types):
-        if (type(speech_types) == float) or (len(speech_types) == 0) or (speech_types == set({None})):
-            s = pd.Series([question_id, list(sentences), [0] * len(sentences)])
-            return pd.DataFrame.from_items(zip(s.index, s.values))
-
-        speech_types = speech_types - set({None})
-        speech_type_target_stat = []
-        for sentence in sentences:
-            have_speech_type = 0
-            for word in tb(sentence).words:
-                speech_type = Utility.get_speech_part(word)
-                if speech_type in speech_types:
-                    have_speech_type = 1
-                    break
-
-            if have_speech_type:
-                speech_type_target_stat.append(1)
-            else:
-                speech_type_target_stat.append(0)
-
-        s = pd.Series([question_id, list(sentences), speech_type_target_stat])
+        sentences = df.sentence
+        question_id = df.question_id
+        sentences_ners = []
+ 
+        for sentence in sentences:   
+             ners = set()
+         
+             dates_matches = dates_extractor(sentence)
+             dates = [token.value for date in dates_matches for token in date.tokens]
+             if len(dates):
+                 ners.add('Date')
+         
+             for w in tb(sentence).words:
+                 tag = morph.parse(w)[0].tag
+                 if ('Name' in tag) or ('Surn' in tag) or ('Patr' in tag): 
+                     ners.add('Per')   
+                 elif 'Geox' in tag:
+                     ners.add('Geox')
+                 elif 'Orgn' in tag:
+                     ners.add('Orgn')
+                 elif ('ROMN' in tag) or (w.split('-')[0].isdigit()) and (len(w.split('-')) > 1):
+                     ners.add('Date')
+                 elif (w not in dates) and (('NUMR' in tag) or ('NUMB' in tag)):
+                     ners.add('Num')
+             
+             sentences_ners.append(copy(ners))
+         
+        s = pd.Series([question_id, sentences, sentences_ners])
         return pd.DataFrame.from_items(zip(s.index, s.values))
 
     @staticmethod
-    def get_ner_target_stat(question_id, sentences, ners):
-        if (type(ners) == float) or (len(ners) == 0) or (ners == set({None})):
-            s = pd.Series([question_id, list(sentences), [0] * len(sentences)])
-            return pd.DataFrame.from_items(zip(s.index, s.values))
+    def get_sentence_ners_indicators(df):
+        for ner in df.sentence_ners:
+             df[ner] = 1
+        return df
+   
+    @staticmethod
+    def get_ners_counts_by_question_type(df, ners):
+        for ner in ners:	
+            df['{}_local'.format(ner)] = df[ner].sum()
+        return df
 
-        ners = ners - set({None})
-        ner_statistic = []
-        for sentence in sentences:
-            have_ner = 0
-            for word in tb(sentence).words:
-                ner = Utility.get_ner(word)
-                if ner in ners:
-                    have_ner = 1
-                    break
-            if have_ner:
-                ner_statistic.append(1)
+    @staticmethod
+    def get_most_freq_ner_question_type(df, ners):
+        df['question_type_ner'] = ners[np.argmax(df[['{}_local'.format(ner) for ner in ners]].values[0])]
+        return df
+
+    @staticmethod
+    def get_sentence_ner_question_type_indicator(df):
+        question_type_ner = df.question_type_ner.values[0]
+        result = []
+        for sentence_ners in df.sentence_ners:
+            if question_type_ner in sentence_ners:
+                result.append(1)
             else:
-                ner_statistic.append(0)
-
-        s = pd.Series([question_id, list(sentences), ner_statistic])
+                result.append(0)
+        s = pd.Series([df.question_id, df.sentence, result])
         return pd.DataFrame.from_items(zip(s.index, s.values))
 
     @staticmethod
-    def get_answer_by_score(data, col_with_score):
-        predict = np.array(list(data["sentence_lem"]))[np.argsort(list(data[col_with_score]))[::-1]][0]
-        answer = data[data.answer_in_sentence == 1].sentence_lem.values[0]
-        return int(answer == predict)
+    def get_sentence_ner_question_type_interactions(x, combs):
+        question_type_ner = x.question_type_ner.values[0]
+        result = {}
+        for sentence_ners in x.sentence_ners:
+            for comb in combs:
+                q, s = comb[0], comb[1]
+                indicator = 0
+                if (q == question_type_ner) and (s in sentence_ners) or (s == question_type_ner) and (q in sentence_ners):
+                    indicator = 1
+                key = '{}_{}'.format(q, s)
+                if key not in result:
+                    result[key] = [indicator]
+                else:
+                    result[key].append(indicator)
+        s = pd.Series([
+            x.question_id,
+            x.sentence,
+        ] + [result['{}_{}'.format(comb[0], comb[1])] for comb in combs]
+        )
+        return pd.DataFrame.from_items(zip(s.index, s.values))
+
+    @staticmethod
+    def get_question_sentence_word2vec_cosine_dist(x):
+        if not hasattr(Utility, 'w2v'):
+            Utility.set_w2v()
+        w2v = Utility.w2v
+
+        sentence_words = x.sentence_lem.split(' ')
+        question_words = x.question_lem.split(' ')
+    
+        sentence_vec = np.zeros_like(w2v['xxxx'])
+        count = 0
+        for w in sentence_words:
+            if w in w2v:
+                sentence_vec += w2v[w]
+                count += 1
+        if count:
+            sentence_vec /= count
+    
+        question_vec = np.zeros_like(w2v['xxxx'])
+        count = 0
+        for w in question_words:
+            if w in w2v: 
+                question_vec += w2v[w]
+                count += 1
+        if count:
+           question_vec /= count
+    
+        x['question_sentence_word2vec_cosine_dist'] = cosine(sentence_vec, question_vec)
+        return x
